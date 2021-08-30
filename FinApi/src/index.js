ad2b5bdb8e2e59d5d99415fa75e29a8186b306e9
Eduardo Seije <eduardoseije@gmail.com>
2021-08-30 10:22:36 -0300

    Início do curso - Fundamentos nodeJS e início da aplicação FinApi realizando a criação de um cadastro de usuário e a regra de negócio que verifica se o cpf já é cadastrado

---
+++ FinApi/src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post('/account',(request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+  if(customerAlreadyExists) {
+    return response.status(400).json({error: "Customer already exists!"});
+  };
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  });
+  return response.status(201).send();
+})
+
+app.listen(3333);
