59be8db8644a439c80f558463a8543dc64778d2b
Eduardo Seije <eduardoseije@gmail.com>
2021-09-27 09:46:30 -0300

    Criando o repositório de categorias

---
+++ RENTALX/src/repositories/CategoriesRepository.ts
@@ -0,0 +1,26 @@
+import { Category } from "../model/category";
+
+// DTO - Data transfer object 
+interface IcreateCategoryDTO {
+  name: string,
+  description: string
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+  constructor() {
+    this.categories = [];
+  }
+  create({ description, name }: IcreateCategoryDTO): void {
+    const category = new Category();
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date()
+    })
+    
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepository };
--- RENTALX/src/routes/categories.routes.ts
+++ RENTALX/src/routes/categories.routes.ts
@@ -1,24 +1,14 @@
 import { Router } from 'express';
-import { v4 as uuidV4 } from 'uuid';
-import { Category } from '../model/category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories: Category[] = [];
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
-  const category = new Category();
-  
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date()
-  })
-  
-  categories.push(category);
-
-  return response.status(201).json({category});
+  categoriesRepository.create({name, description});
+  return response.status(201).send();
 });
 
 
