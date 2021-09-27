c7c9f581294268d11ef8392d80ec8943c1490ba7
Eduardo Seije <eduardoseije@gmail.com>
2021-09-27 10:08:33 -0300

    Criação do endpoint para listar todas as categorias cadastradas

--- RENTALX/src/repositories/CategoriesRepository.ts
+++ RENTALX/src/repositories/CategoriesRepository.ts
@@ -21,6 +21,10 @@ class CategoriesRepository {
     
     this.categories.push(category);
   }
-}
+  list(): Category[] {
+    return this.categories;
+  };
+};
+
 
 export { CategoriesRepository };
